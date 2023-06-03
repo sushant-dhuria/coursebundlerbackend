7f36de41acdfe3d56523e218ff6b2f9dc608787c
sushant-dhuria <sushantdhuria123@gmail.com>
2023-06-03 11:26:36 +0530

    backend added to deploy

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import app from "./app.js";
+import { connectDB } from "./config/database.js";
+import cloudinary from "cloudinary";
+import RazorPay from "razorpay";
+import {config} from "dotenv";
+import nodeCron from "node-cron";
+import { Stats } from "./models/Stats.js";
+connectDB();
+
+config({
+  path: "./config/config.env",
+});
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
+  api_key: process.env.CLOUDINARY_CLIENT_API,
+  api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
+});
+
+export const instance = new RazorPay({
+  key_id: process.env.RAZORPAY_API_KEY,
+  key_secret: process.env.RAZORPAY_API_SECRET,
+});
+
+nodeCron.schedule("0 0 0 5 * *", async () => {
+  try {
+    await Stats.create({});
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is working on port: ${process.env.PORT}`);
+});
